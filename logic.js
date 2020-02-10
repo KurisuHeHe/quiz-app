35c15e6f49908cf2db5529b01dc811f7b5caed6d
Chris Ramirez <chris.ramirez163@gmail.com>
2020-02-10 22:55:37 +0000

    updated all files

---
+++ logic.js
@@ -0,0 +1,128 @@
+function startQuiz() {
+   $('.larger-container').on('click', '.inner-button', function (event){
+       $('.container').hide()
+       createForm();
+   });
+}
+
+
+//purpose of this function is to loop through the object STORE and populate the values for each question
+function createChoices() {
+    let question = STORE.questions[STORE.currentQuestion];
+    for(let i=0; i<question.choices.length; i++){
+        $('.optionContainer').append(`
+        <input type='radio' name='options' id='option${i + 1}' value='${question.choices[i]}' tabindex="1">
+        <label for='option${i + 1}'>${question.choices[i]}</label>
+        <br>
+        `);
+    }
+}
+
+function updateCounters(){
+    $('.js-number').text(`Question #: ${STORE.currentQuestion + 1}/${STORE.questions.length}`);
+    $('.js-score').text(`Current Score: ${STORE.score}/${STORE.questions.length}`);
+}
+
+
+function finalPage(){
+    $('.questionContainer').hide();
+    $('.optionContainer').hide();
+    $('.submitContainer').hide();
+    $('.finalScore').append(`
+    <p>Hope you learned everything you needed for the real test!</p>
+    <p>Final Score</p>
+    ${STORE.score}/${STORE.questions.length}
+    <br>
+    <button type='button' id='restart' tabindex="7">Restart</button>`
+    )
+};
+
+
+
+//function is used for inserting the form onto the html
+function createForm() {
+    let question = STORE.questions[STORE.currentQuestion];
+        let questionnaire = $(
+            `<form class='js-form'>
+                <fieldset>
+                    <div class='questionContainer'>
+                        <legend class='questionLegend'>${question.question}</legend>
+                    </div>
+                    <div class='optionContainer'>
+                    </div>
+                    <div class='submitContainer'>
+                        <button typ='submit' id='answer' tabindex="5">Submit</button>
+                        <button type='button' id='next' tabindex='6'>Next</button>
+                    </div>
+                    <div class='finalScore'>
+                    </div>
+                </fieldset>
+            </form>`
+        );
+    $('main').html(questionnaire);
+    createChoices();
+    updateCounters();
+    $('#next').hide();
+}
+
+function nextButton(){
+    $('body').on('click', '#next', function(event){
+        event.preventDefault();
+        if(STORE.currentQuestion === 10){
+            finalPage();
+        }
+        else {
+            STORE.currentQuestion++;
+        createForm();
+        }
+    });
+}
+
+
+//purpose of this function is to capture the value of the checked choice then compare it to the correctAnswer in the array. Based on the answer it will prompt either right or wrong
+//the counter for currentQuestion should increase and also bring back the next button
+function submitButton(){
+    $('body').on('submit', '.js-form', function(event) {
+        event.preventDefault();
+        let curQues = STORE.questions[STORE.currentQuestion];
+        let pickedChoice = $('input:checked');
+        let pickedValue = pickedChoice.val();
+        let correct = STORE.questions[STORE.currentQuestion].correctAnswer;
+        if(!pickedValue){
+            alert('Please choose an option');
+            return;
+        }
+        if(pickedValue === correct){
+            alert('wow you got it right');
+            STORE.score++;
+        }
+        else {
+            alert(`you suck , the right answer is "${correct}"`);
+        }
+        $('#answer').hide();
+        $('#next').show();
+    })
+}
+
+
+function restart(){
+    $('body').on('click', '#restart', function(event){
+        event.preventDefault();
+        STORE.currentQuestion = 0
+        STORE.score = 0
+        createForm();
+    })
+}
+
+
+function handleQuizApp() {
+    startQuiz();
+    submitButton();
+    nextButton();
+    restart();
+}
+
+
+$(handleQuizApp);
+
+
---
+++ store.js
@@ -0,0 +1,126 @@
+const STORE = {
+    questions: [
+    {
+      question:
+       "The minimum age for a New York real estate broker's license?",
+      choices: [
+        '19',
+        '18',
+        '20',
+        '21'
+      ],
+      correctAnswer:
+        '20'
+    },
+    {
+      question:
+        "A Salesperson’s license must be",
+      choices: [
+        'Displayed on the firm’s wall',
+        'Kept in the salesperson’s wallet',
+        'Retained by the principal broker',
+        'Sent to the local realtors association'
+      ],
+      correctAnswer:
+        'Retained by the principal broker'
+    },
+    {
+        question:
+        "When a license is revoked, how long must a licensee wait before applying to have the license reinstated?",
+        choices: [
+            '18 months',
+            '1 year',
+            '6 months',
+            '2 years'
+        ],
+        correctAnswer:
+        '1 year'
+    },
+    {
+        question:
+        "A deed is valid when it is",
+        choices: [
+            'Delivered and accepted',
+            'Recorded',
+            'Signed',
+            'Notarized'
+        ],
+        correctAnswer:
+        'Delivered and accepted'
+    },
+    {
+        question:
+        "A lien is an:",
+        choices: [
+            'Appurtenance',
+            'Encroachment',
+            'Encumbrance',
+            'Eminent domain'
+        ],
+        correctAnswer:
+        'Encumbrance'
+    },
+    {
+        question:
+        "Broker John Brown owes customer Susie Smith",
+        choices: [
+            'Loyalty',
+            'Fairness and Honesty',
+            'Confidentiality',
+            'Accounting'
+        ],
+        correctAnswer:
+        'Fairness and Honesty'
+    },
+    {
+        question:
+        "Salesperson need not be",
+        choices: [
+            'Licensed by the state',
+            'At least 18 years old',
+            'Already experienced in real estate',
+            'Supervised by a broker'
+        ],
+        correctAnswer:
+        'Already experienced in real estate'
+    },
+    {
+        question:
+        "When serving as a buyer's agent, according to the Civil Code sections dealing with agency, a licensee owes a fidicuary duty:",
+        choices: [
+            'The buyer',
+            'The Seller',
+            'The broker',
+            'The buyer & seller'
+        ],
+        correctAnswer:
+        'The buyer'
+    },
+    {
+        question:
+        "In a real estate transaction, fiduciary duties are owed to the:",
+        choices: [
+            'Principal',
+            'Customer',
+            'Broker',
+            'Agent'
+        ],
+        correctAnswer:
+        'Principal'
+    },
+    {
+        question:
+        "If a salesperson acts as an independent contractor, then:",
+        choices: [
+            'He controls his own working hours',
+            'All of his listing belongs to him',
+            'His broker must withold income tax from his commission checks',
+            'He is responsible to his broker as to how his work is performed'
+        ],
+        correctAnswer:
+        'He controls his own working hours'
+    }
+],
+currentQuestion: 0,
+score: 0,
+};
